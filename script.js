9b9807409f8281f10565983aad2b742f79db94fc
frogsaurus <autobadin@gmail.com>
2024-12-15 16:12:57 +0700

    select

---
+++ script.js
@@ -0,0 +1,7 @@
+//select
+const operatorButton = document.querySelectorAll("#btnOp");
+const numberButton = document.querySelectorAll("#btnNum");
+const equalButton = document.querySelector("#btnEqual");
+const ClearButton = document.querySelector("#btnClear");
+const deleteButton = document.querySelector("#btnDelete");
+
